ba574c833af3a501bda74bacd36c3597f5c0aa30
langlang <785218577@qq.com>
2017-04-09 20:00:25 +0800

    goods api

--- advanced/frontend/modules/api/modules/v1/controllers/GoodsController.php
+++ advanced/frontend/modules/api/modules/v1/controllers/GoodsController.php
@@ -2,19 +2,31 @@
 namespace v1\controllers;
 
 use yii\rest\ActiveController;
+use v1\models\Goods;
 
 class GoodsController extends ActiveController
 {
     public $modelClass = 'v1\models\Goods';
 
-    public function actions()
-    {
-        $actions = parent::actions();
-        // // 禁用"delete" 和 "create" 动作
-        // unset($actions['delete'], $actions['create']);
+    // public function actions()
+    // {
+    //     $actions = parent::actions();
+    //     // // 禁用"delete" 和 "create" 动作
+    //     // unset($actions['delete'], $actions['create']);
 
-        // // 使用"prepareDataProvider()"方法自定义数据provider 
-        // $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
-        return $actions;
+    //     // // 使用"prepareDataProvider()"方法自定义数据provider 
+    //     // $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+    //     return $actions;
+    // }
+    
+    /**
+     * 首页商品推荐
+     */
+    public function actionRecommend(){
+        $goods = Goods::find()->select(['id','name','sale_price','(virtual_nums + volume) as volume'])
+            ->where(['status'=>0,'recommend'=>1])
+            ->limit(4)
+            ->all();
+        return $goods;
     }
 }
--- advanced/frontend/modules/api/modules/v1/controllers/HomePageController.php
+++ advanced/frontend/modules/api/modules/v1/controllers/HomePageController.php
@@ -3,14 +3,13 @@ namespace v1\controllers;
 
 use yii\rest\Controller;
 use app\models\Goods;
+use yii\helpers\ArrayHelper;
 
 class HomePageController extends Controller
 {
+    
     public function actionGoods(){
-        $goods = Goods::find()->select(['id','name','sale_price','(virtual_nums + volume) as volume'])
-            ->where(['status'=>0,'recommend'=>1])
-            ->limit(4)
-            ->all();
+        
         return $goods;
     }
 }
